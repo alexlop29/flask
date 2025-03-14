f57d5976f6bfee979f1789782123bb3218d1f3dd
Alexander Lopez <alexanderlopez@macbook-pro.mynetworksettings.com>
2025-03-14 11:49:15 -0400

    Handle various HTTP methods, input sanitization, and template rendering

--- README.md
+++ README.md
@@ -15,8 +15,16 @@ Enable debug mode to automatically reload the server with changes.
 flask run --debug
 ```
 
-### Security
-HTML is the default flask response type.
+Tips
+- Use the route() decorator to bind a function to a URL.
+- You can add variable sections to a URL by marking sections with <variable_name>. 
+- The `url_for()` function is very useful for dynamically building a URL for a specific function.
+- Flask configures the Jinja2 template engine for you automatically.
 
-Leverage HTML escaping for any user-provided values to prevent
-injection attacks.
+### Security
+- HTML is the default flask response type. Leverage HTML escaping for any user-provided values to prevent injection attacks.
+
+### Resources
+- [Flask - Docs](https://flask.palletsprojects.com/en/stable/)
+- [Jinga - Docs](https://palletsprojects.com/projects/jinja/)
+- [TutorialsPoint - Flask - URL Building](https://www.tutorialspoint.com/flask/flask_url_building.htm)
--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
+# Importing escape from markupsafe to sanitize user input
+from markupsafe import escape
+
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Demonstrates a simple route
 @app.route('/')
 def hello_world():
     return '<p>Hello, Alex!</p>'
+
+# Demonstrates sanitization of user input
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+# Demonstrates handling various HTTP methods in a route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Handling POST request"
+    else:
+        return "Handling GET request"
+
+# Demonstrates an alternative way to handle different HTTP methods
+@app.get("/login2")
+def login_get():
+    return "Handling GET request"
+@app.post("/login2")
+def login_post():
+    return "Handling POST request"
+
+# Demonstrates rendering a template
+@app.route("/hello/<name>")
+def hello_with_template(name=None):
+    return render_template("hello.html", person=name)
